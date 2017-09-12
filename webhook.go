7e80cd4b4f94d2b2cf45f48023017d9b6a2ea945
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-12 18:13:42 +0900

    Replace int type to Number struct type

--- webhook.go
+++ webhook.go
@@ -2,7 +2,9 @@ package webhook
 
 import (
 	"encoding/binary"
+	"math"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -32,18 +34,66 @@ type Role struct {
 }
 
 type Alert struct {
-	URL               string  `json:"url"`
-	CreatedAt         Time    `json:"createdAt"`
-	Status            string  `json:"status"`
-	IsOpen            bool    `json:"isOpen"`
-	Trigger           string  `json:"trigger"`
-	MonitorName       string  `json:"monitorName"`
-	MetricLabel       string  `json:"metricLabel"`
-	MetricValue       float64 `json:"metricValue"`
-	CriticalThreshold int     `json:"criticalThreshold"`
-	WarningThreshold  int     `json:"warningThreshold"`
-	MonitorOperator   string  `json:"monitorOperator"`
-	Duration          int     `json:"duration"`
+	URL               string `json:"url"`
+	CreatedAt         Time   `json:"createdAt"`
+	Status            string `json:"status"`
+	IsOpen            bool   `json:"isOpen"`
+	Trigger           string `json:"trigger"`
+	MonitorName       string `json:"monitorName"`
+	MetricLabel       string `json:"metricLabel"`
+	MetricValue       Number `json:"metricValue"`
+	CriticalThreshold Number `json:"criticalThreshold"`
+	WarningThreshold  Number `json:"warningThreshold"`
+	MonitorOperator   string `json:"monitorOperator"`
+	Duration          Number `json:"duration"`
+}
+
+type Number struct {
+	AsInt64   *int64
+	AsFloat64 *float64
+}
+
+func NewInt64AsNumber(i int64) *Number {
+	return &Number{AsInt64: &i, AsFloat64: nil}
+}
+
+func NewFloat64AsNumber(f float64) *Number {
+	return &Number{AsInt64: nil, AsFloat64: &f}
+}
+
+func (t *Number) MarshalJSON() ([]byte, error) {
+	u := make([]byte, 0, 8)
+	if t.AsFloat64 != nil {
+		binary.LittleEndian.PutUint64(u, math.Float64bits(*t.AsFloat64))
+	} else if t.AsInt64 != nil {
+		binary.LittleEndian.PutUint64(u, uint64(*t.AsInt64))
+	}
+
+	return u, nil
+}
+
+func (t *Number) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+	var err error
+	rawNumber := string(data)
+	if strings.Contains(rawNumber, ".") {
+		f, err := strconv.ParseFloat(rawNumber, 64)
+		if err != nil {
+			return err
+		}
+		t.AsFloat64 = &f
+		t.AsInt64 = nil
+	} else {
+		i, err := strconv.ParseInt(rawNumber, 10, 64)
+		if err != nil {
+			return err
+		}
+		t.AsInt64 = &i
+		t.AsFloat64 = nil
+	}
+	return err
 }
 
 type Time struct {
--- webhook_test.go
+++ webhook_test.go
@@ -59,3 +59,62 @@ func TestUnmarshal(t *testing.T) {
 		t.Fatalf("unexpected error: 2017-08-02T02:57:01+09:00 != %s", ctime)
 	}
 }
+
+func TestMarshalNumber(t *testing.T) {
+	rawJson := `
+{
+	"orgName": "TestOrg",
+	"event": "alert",
+	"host": {
+		"name": "epy0n0ff-TestPC",
+		"url": "https://mackerel.io/orgs/TestOrg/hosts/32subXxxxxx",
+		"isRetired": false,
+		"id": "32subXxxxxx",
+		"status": "working",
+		"memo": "",
+		"roles": [{
+				"fullname": "TestSub: TestSubHDD",
+				"serviceName": "TestSub",
+				"roleName": "TestSubHDD",
+				"serviceUrl": "https://mackerel.io/orgs/Test/services/TestSub",
+				"roleUrl": "https://mackerel.io/orgs/Test/services/TestSub#role=TestSubHDD"
+			},
+			{
+				"fullname": "TestNetwork: TestNetwork",
+				"serviceName": "TestNetwork",
+				"roleName": "TestNetwork",
+				"serviceUrl": "https://mackerel.io/orgs/TestOrg/services/TestNetwork",
+				"roleUrl": "https://mackerel.io/orgs/TestOrg/services/TestNetwork#role=TestNetwork"
+			}
+		]
+	},
+	"alert": {
+		"url": "https://mackerel.io/orgs/TestOrg/alerts/359Mwoxxxxx",
+		"createdAt": 1501610221657,
+		"status": "ok",
+		"isOpen": false,
+		"trigger": "delete monitor",
+		"monitorName": "Memory %テスト監視",
+		"metricLabel": "Memory %",
+		"criticalThreshold": 20,
+		"warningThreshold": 1.4665636369580741,
+		"monitorOperator": ">",
+		"duration": 1
+	}
+}`
+
+	var hook WebHook
+	err := json.Unmarshal([]byte(rawJson), &hook)
+	if err != nil {
+		t.Fatalf("unexpected error:%v", err)
+	}
+
+	if 20 != *hook.Alert.CriticalThreshold.AsInt64 {
+		t.Fatalf("unexpected error: criticalThreshold: 20 != %d", *hook.Alert.CriticalThreshold.AsInt64)
+	}
+
+	if 1.4665636369580741 != *hook.Alert.WarningThreshold.AsFloat64 {
+		t.Fatalf("unexpected error: warningThreshold: 1.4665636369580741 != %f", *hook.Alert.WarningThreshold.AsFloat64)
+	}
+
+}
